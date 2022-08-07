c694a7834146a40478a57b5412dd03456569ce68
Hagar Mohamed <HagarMohamed2399@gmail.com>
2022-08-07 15:30:16 +0200

    Online License

--- Keygen/Keygen/LicenseManagment.cs
+++ Keygen/Keygen/LicenseManagment.cs
@@ -14,6 +14,24 @@ namespace Keygen
         {
             client = new RestClient($"https://api.keygen.sh/v1/accounts/{accountId}");
         }
+        public async Task<Document<License, Validation>> ValidateLicense(string license_id)
+        {
+            var request = new RestRequest($"licenses/{license_id}/actions/validate", Method.Post);
+            request.AddHeader("Content-Type", "application/vnd.api+json");
+            request.AddHeader("Accept", "application/vnd.api+json");
+            request.AddHeader("Authorization", $"Bearer admin-e259cf4d650f73e54e68384dac0cf804aee9dfde6b650d8eca444818185b1d1bv3");
+
+
+            var response = await client.ExecuteAsync<Document<License, Validation>>(request);
+            if (response.Data.Errors.Count > 0)
+            {
+                var err = response.Data.Errors[0];
+
+                Console.WriteLine("[ERROR] [ValidateLicense] Status={0} Title={1} Detail={2} Code={3}", response.StatusCode, err.Title, err.Detail, err.Code);
+            }
+
+            return response.Data;
+        }
 
     }
 }
--- Keygen/Keygen/Program.cs
+++ Keygen/Keygen/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using Keygen;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -6,9 +7,23 @@ namespace keygen
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            License license = null;
+            Machine device = null;
+            var license_managment = new LicenseManagement("fb252870-86a1-4007-9bc5-c3e127ab7871");
+            string license_id = "2b022c6c-cd94-481b-8a14-253b639f3656";
 
+            //Validate License
+            var validation = await license_managment.ValidateLicense(license_id);
+            if (validation.Meta.Valid)
+            {
+                Console.WriteLine("[INFO] [ValidateLicense] Valid={0} ValidationCode={1}", validation.Meta.Detail, validation.Meta.code);
+            }
+            else
+            {
+                Console.WriteLine("[INFO] [ValidateLicense] Invalid={0} ValidationCode={1}", validation.Meta.Detail, validation.Meta.code);
+            }
         }
     }
 }
