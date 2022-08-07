ba787f923f740b553573091d8d142d8e52c367e7
Hagar Mohamed <HagarMohamed2399@gmail.com>
2022-08-07 15:11:40 +0200

    start keygen

---
+++ Keygen/Keygen/Document.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keygen
+{
+    public class Document<T>
+    {
+        public T Data { get; set; }
+        public List<Error> Errors { get; set; } = new();
+    }
+
+    public class Document<T, U> : Document<T>
+    {
+        public U Meta { get; set; }
+    }
+
+    public class Error
+    {
+        public string Title { get; set; }
+        public string Detail { get; set; }
+        public string Code { get; set; }
+    }
+    public class License
+    {
+        public string Type { get; set; }
+        public string ID { get; set; }
+    }
+    public class Validation
+    {
+        public bool Valid { get; set; }
+        public string Detail { get; set; }
+        public string code { get; set; }
+    }
+    public class Machine
+    {
+        public string Type { get; set; }
+        public string ID { get; set; }
+    }
+    public class LicenseFile
+    {
+        public string enc { get; set; }
+        public string sig { get; set; }
+        public string alg { get; set; }
+    }
+}
---
+++ Keygen/Keygen/LicenseManagment.cs
@@ -0,0 +1,19 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keygen
+{
+    public class LicenseManagement
+    {
+        RestClient? client = null;
+        public LicenseManagement(string accountId)
+        {
+            client = new RestClient($"https://api.keygen.sh/v1/accounts/{accountId}");
+        }
+
+    }
+}
