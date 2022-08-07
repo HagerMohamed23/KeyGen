ae4f9a10f127ecb6958daa0d60742f66d38bf341
Hagar Mohamed <HagarMohamed2399@gmail.com>
2022-08-07 15:43:01 +0200

    decrypt offline license

--- Keygen/Keygen/LicenseManagment.cs
+++ Keygen/Keygen/LicenseManagment.cs
@@ -1,8 +1,10 @@
-﻿using RestSharp;
+﻿using Newtonsoft.Json;
+using RestSharp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Keygen
@@ -31,6 +33,103 @@ namespace Keygen
             }
 
             return response.Data;
+        }
+        public async Task<Document<Machine>> ActivateDevice(string licenseId, string deviceFingerprint, string activationToken)
+        {
+            var request = new RestRequest("machines", Method.Post);
+
+            request.AddHeader("Authorization", $"Bearer {activationToken}");
+            request.AddHeader("Content-Type", "application/vnd.api+json");
+            request.AddHeader("Accept", "application/vnd.api+json");
+            request.AddJsonBody(new
+            {
+                data = new
+                {
+                    type = "machine",
+                    attributes = new
+                    {
+                        fingerprint = deviceFingerprint,
+
+                    },
+                    relationships = new
+                    {
+                        license = new
+                        {
+                            data = new
+                            {
+                                type = "license",
+                                id = licenseId,
+                            }
+                        }
+                    }
+                }
+            });
+
+            var response = await client.ExecuteAsync<Document<Machine>>(request);
+            if (response.Data.Errors.Count > 0)
+            {
+                var err = response.Data.Errors[0];
+
+                Console.WriteLine("[ERROR] [ActivateDevice] Status={0} Title={1} Detail={2} Code={3}", response.StatusCode, err.Title, err.Detail, err.Code);
+
+                Environment.Exit(1);
+            }
+
+            return response.Data;
+        }
+        public string GenerateLicenseToken(string license_id)
+        {
+            var request = new RestRequest(
+              $"licenses/{license_id}/tokens",
+              Method.Post
+            );
+
+            request.AddHeader("Content-Type", "application/vnd.api+json");
+            request.AddHeader("Accept", "application/vnd.api+json");
+            request.AddHeader("Authorization", "Bearer admin-e259cf4d650f73e54e68384dac0cf804aee9dfde6b650d8eca444818185b1d1bv3");
+
+            request.AddJsonBody(new
+            {
+                data = new
+                {
+                    type = "tokens",
+                    attributes = new
+                    {
+                        maxActivations = 1
+                    }
+                }
+            });
+
+            var response = client.Execute(request);
+            dynamic json = JsonConvert.DeserializeObject(response.Content);
+            string activation_token = json["data"]["attributes"]["token"];
+            return activation_token;
+        }
+        public string CreateLicenseFile(string license_id)
+        {
+            var request = new RestRequest($"licenses/{license_id}/actions/check-out", Method.Get);
+            request.AddHeader("Accept", "application/vnd.api+json");
+            request.AddHeader("Authorization", $"Bearer admin-e259cf4d650f73e54e68384dac0cf804aee9dfde6b650d8eca444818185b1d1bv3");
+
+            var response = client.Execute(request);
+
+            return response.Content;
+        }
+        public string DecryptLicenseFile(string path)
+        {
+            string licenseFile = System.IO.File.ReadAllText(path);
+            var encodedPayload = Regex.Replace(licenseFile, Environment.NewLine, String.Empty);
+            encodedPayload = Regex.Replace(encodedPayload, "(^-----BEGIN LICENSE FILE-----|-----END LICENSE FILE-----$)", "");
+            var payloadBytes = Convert.FromBase64String(encodedPayload);
+            var payload = Encoding.UTF8.GetString(payloadBytes);
+
+            var lic = JsonConvert.DeserializeObject<LicenseFile>(payload);
+
+            if (lic.alg != "base64+ed25519")
+                Console.WriteLine("Unsupported Algorithm");
+
+            string data = Encoding.UTF8.GetString(Convert.FromBase64String(lic.enc));
+            return data;
         }
 
     }
--- Keygen/Keygen/Program.cs
+++ Keygen/Keygen/Program.cs
@@ -9,8 +9,8 @@ namespace keygen
     {
         static async Task Main(string[] args)
         {
-            License license = null;
-            Machine device = null;
+            License? license = null;
+            Machine? device = null;
             var license_managment = new LicenseManagement("fb252870-86a1-4007-9bc5-c3e127ab7871");
             string license_id = "2b022c6c-cd94-481b-8a14-253b639f3656";
 
@@ -23,6 +23,42 @@ namespace keygen
             else
             {
                 Console.WriteLine("[INFO] [ValidateLicense] Invalid={0} ValidationCode={1}", validation.Meta.Detail, validation.Meta.code);
+            }
+
+            //Create License File and save it
+            string test = license_managment.CreateLicenseFile(license_id);
+            using (FileStream fs = File.Create(@"D:\Learning\KeyGen\Keygen\Keygen\license.txt"))
+            {
+                char[] value = test.ToCharArray();
+                fs.Write(Encoding.UTF8.GetBytes(value), 0, value.Length);
+            }
+
+            license = validation.Data;
+            //Check if machine is activated
+            switch (validation.Meta.code)
+            {
+                case "FINGERPRINT_SCOPE_MISMATCH":
+                case "NO_MACHINES":
+                case "NO_MACHINE":
+                    var activation_token = license_managment.GenerateLicenseToken(license_id);
+                    var activation = await license_managment.ActivateDevice(license_id, "", activation_token);
+
+                    device = activation.Data;
+
+                    Console.WriteLine("[INFO] [ActivateDevice] DeviceId={0} LicenseId={1}", device.ID, license.ID);
+
+                    validation = await license_managment.ValidateLicense(license_id);
+                    if (validation.Meta.Valid)
+                    {
+                        Console.WriteLine("[INFO] [ValidateLicense] Valid={0} ValidationCode={1}", validation.Meta.Detail, validation.Meta.code);
+                    }
+                    else
+                    {
+                        Console.WriteLine("[INFO] [ValidateLicense] Invalid={0} ValidationCode={1}", validation.Meta.Detail, validation.Meta.code);
+                    }
+
+                    break;
+                    Console.WriteLine("[INFO] [Main] Valid={0} RecentlyActivated={1}", validation.Meta.Valid);
             }
         }
     }
